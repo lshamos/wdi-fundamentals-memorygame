e94278ad77c22a1cbd9f3f1c0f73455ba9e7e9a1
lshamos <lshamos@gmail.com>
2017-04-21 08:14:16 -0700

    created game logic

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 "main.js"
 
-var cardOne = "queen" ;
-var cardTwo = "king" ;
-var cardThree = "queen" ;
-var cardFour = "king" ;
+console.log("User flipped queen");
+console.log("User flipped king");
 
-console.log ("User flipped " + cardOne);
-console.log ("User flipped " + cardTwo);
- 
+ var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [0]; 
+cardsInPlay.push('queen');
+cardsInPlay.push('king');
+var cardOne = [0];
+var cardTwo = [2];
+
+if(cardsInPlay[0] === [1]) {alert("You found a match!");
+}
+	else {
+		alert("Sorry try again.");
+	}
