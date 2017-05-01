e7512147c0d32fc156ff73dc9cd3c1b8baa0fc8d
lshamos <lshamos@gmail.com>
2017-05-01 12:08:36 -0700

    created game logic

--- main.js
+++ main.js
@@ -25,19 +25,34 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-if (cardsInPlay[0] === [1]) {
+if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!"); 
 }	else { 
 	alert("Sorry try again.");
 	}
 };
 
-var flipCard = function(cardId) {
-	checkForMatch(); 
-	console.log("User flipped " + cards[cardId].rank);
-cardsInPlay.push(cards[cardId].rank);
+var flipCard = function() {
+var cardId = this.getAttribute('data-id');
+console.log("User flipped " + cards[cardId].rank);	 
 console.log(cards[cardId].suit);
 console.log(cards[cardId].cardImage);
+cardsInPlay.push("queen");
+
+this.setAttribute('src', cards[cardId].cardImage);
 };
-flipCard(0);
-flipCard(2);
+checkForMatch(); 
+
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+var cardElement = document.createElement('img');
+cardElement.setAttribute('src', "images/back.png");
+cardElement.setAttribute('data-id', i);
+cardElement.addEventListener('click', flipCard);
+
+document.getElementById("game-board").appendChild(cardElement);
+}
+};
+
+createBoard();
